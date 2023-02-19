fb0df2568cd32d987bef1c9c187668f8beae0fc3
Sharif Uddin <uddin15-4475@diu.edu.bd>
2023-02-19 23:09:56 +0600

    Delegate Event

--- delegate.js
+++ delegate.js
@@ -1,6 +1,14 @@
+// delete
 const items = document.getElementsByClassName('item');
 for (const item of items) {
     item.addEventListener('click', function (event) {
         event.target.parentNode.removeChild(event.target);
     });
 }
+// Addd
+document.getElementById('add-item').addEventListener('click', function () {
+    const li = document.createElement('li');
+    li.innerText = 'Brand New Item';
+    const parent = document.getElementById('item-container');
+    parent.appendChild(li);
+})
